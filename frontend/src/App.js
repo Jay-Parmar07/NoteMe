deefd0edebe1c6e56c56d423ee1de39dc77a02d3
Jay-Parmar07 <jay7604parmar@gmail.com>
2025-12-20 16:17:28 +0530

    fix: correct import casing for Header/Footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
 //instead of div we can use React fragment <> </> also to get rid of return statement we can use arrow function with () brackets
 
 import './App.css';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+
 
 const App=() => 
    (
-    <h1 className="App">
-      Hello World 
-    </h1>
+    <>
+    <Header/>
+    <Footer/>
+    </>
   );
 
 
